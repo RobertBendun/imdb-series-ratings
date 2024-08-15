50f393cd9f52a015af6c7989d6efc177934e9c2f
Robert Bendun <robert@bendun.cc>
2024-08-15 13:09:50 +0200

    CLI refactor, added completion mode

--- README.md
+++ README.md
@@ -2,10 +2,33 @@
 
 Using [IMDb dataset](https://www.imdb.com/interfaces/) to create some stats about series
 
-## Example
+## Usage
+
+First install dependencies:
 
 ```console
-$ poetry run python ./imdb-series-ratings.py -i 'tt3107288' --box
+poetry install
+```
+
+To see how much of given show have you already watched (and what you have left):
+
+```console
+poetry run python imdb-series-ratings.py -n "Community" completion -s 4 -e 2
+Completed 72 episodes with 38 left from 110 episodes total (65.45%)
+Watched 1580 minutes (26h 20min) with 895 minutes left (14h 55min) from 2475 minutes total (26h 20min) (63.84%)
+Series episodes were released in 2009-2015
+
+- In season 4 (2013) there are 12 remaining episodes with total runtime of 261 minutes (4h 21min) (10.55% of whole series)
+- In season 5 (2014) there are 13 remaining episodes with total runtime of 272 minutes (4h 32min) (10.99% of whole series)
+- In season 6 (2015) there are 13 remaining episodes with total runtime of 362 minutes (6h 2min) (14.63% of whole series)
+```
+
+Note that depending on your computer performance it may take a while (~2mins).
+
+To show box plot of ratings from series seasons:
+
+```console
+poetry run python imdb-series-ratings.py -i 'tt3107288' box
 ```
 
 ![Boxplot about Flash TV show](./flash.png)
--- imdb-series-ratings.py
+++ imdb-series-ratings.py
@@ -1,5 +1,9 @@
+# TODO: Add by episode breakdown for completion mode
+# TODO: runtime estimation doesn't work for last season of Doom Patrol, don't report it when there are NA's
 import argparse
+import csv
 import gzip
+import itertools
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
@@ -14,13 +18,16 @@ TSV_FILES = [
     "title.ratings.tsv",
 ]
 
+verbosity_level = 0
+
 
 def error(*args, **kwargs):
     print("[ERROR]", *args, **kwargs, file=sys.stderr)
 
 
 def info(*args, **kwargs):
-    print("[INFO]", *args, **kwargs)
+    if verbosity_level > 0:
+        print("[INFO]", *args, **kwargs)
 
 
 def download_dataset():
@@ -36,31 +43,34 @@ def download_dataset():
                 shutil.copyfileobj(f_in, f_out)
 
 
-def load_basics(usecols):
+def load_basics(usecols=None):
     info("Loading title.basics.tsv")
-    return pd.read_csv(
+    return pd.read_table(
         "title.basics.tsv",
-        sep="\t",
         dtype={
-            "tconst": "object",
-            "titleType": "object",
-            "primaryTitle": "object",
-            "originalTitle": "object",
-            "isAdult": "object",
+            "tconst": str,
+            "titleType": str,
+            "primaryTitle": str,
+            "originalTitle": str,
+            "isAdult": str,
             "startYear": "Int64",
             "endYear": "Int64",
             "runtimeMinutes": "Int64",
-            "genres": str,
+            "genres": "object",
         },
+        header=0,
         na_values={"\\N"},
         index_col=0,
+        low_memory=False,
         usecols=usecols,
+        keep_default_na=False,
+        quoting=csv.QUOTE_NONE,
     )
 
-def main(args):
-    download_dataset()
 
-    title = None
+def resolve_imdb_id_and_title(args) -> tuple[str, str]:
+    # TODO: This is a code smell, the caller should decide what they want
+    title_required = args.mode in ("box", "episodes")
 
     if args.name is not None:
         basics = load_basics(usecols=[0, 1, 2, 5, 6])
@@ -85,14 +95,52 @@ def main(args):
 
         chosen = matched.index[chosen]
         title = matched.at[chosen, "primaryTitle"]
+        return (chosen, title)
     elif args.id is not None:
         chosen = args.id
+        if title_required:
+            title = load_basics(usecols=[0, 2]).at[chosen, "primaryTitle"]
+            return (chosen, title)
+        else:
+            return (chosen, None)
     else:
         error("Expected either --id or --name")
         exit(2)
 
-    if title is None:
-        title = load_basics(usecols=[0, 2]).at[chosen, "primaryTitle"]
+
+def hours_minutes(minutes: int) -> str:
+    assert minutes >= 0, "pre: minutes must be >= 0"
+
+    hours = minutes // 60
+    minutes %= 60
+    hours = [f"{hours}h"] if hours > 0 else []
+    minutes = [f"{minutes}min"] if minutes > 0 else []
+    return f" ({result})" if (result := " ".join([*hours, *minutes])) else ""
+
+
+def collapse_years(years: list[int]) -> str:
+    assert all(a <= b for a, b in zip(years, years[1:])), "pre: years must be sorted"
+
+    def do():
+        start = years[0]
+        previous = years[0]
+        for year in itertools.chain(years[1:], (None,)):
+            if year == previous + 1:
+                previous = year
+                continue
+            if start == previous:
+                yield f"{start}"
+            else:
+                yield f"{start}-{previous}"
+            start = year
+            previous = year
+
+    return ", ".join(do())
+
+
+def main(args):
+    download_dataset()
+    id, title = resolve_imdb_id_and_title(args)
 
     info("Loading episode titles")
     episode = pd.read_csv(
@@ -106,32 +154,85 @@ def main(args):
         },
         na_values={"\\N"},
         index_col=0,
+        low_memory=False,
     )
 
-    info("Loading ratings")
-    ratings = pd.read_csv(
-        "title.ratings.tsv",
-        sep="\t",
-        dtype={"tconst": "string", "averageRating": "Float32", "numVotes": "Int64"},
-        index_col=0,
-    )
+    if args.mode in ("box", "episodes"):
+        # TODO: Code smell
+        info("Loading ratings")
+        ratings = pd.read_csv(
+            "title.ratings.tsv",
+            sep="\t",
+            dtype={"tconst": "string", "averageRating": "Float32", "numVotes": "Int64"},
+            index_col=0,
+            low_memory=False,
+        )
 
-    episodes_rated = episode[episode["parentTconst"] == chosen].join(ratings)
-
-    plt.title(title)
-
-    if args.box:
+    if args.mode == "box":
+        episodes_rated = episode[episode["parentTconst"] == id].join(ratings)
+        plt.title(title)
         sns.boxplot(x="seasonNumber", y="averageRating", data=episodes_rated)
         plt.xlabel("Season number")
         plt.ylabel("Average episode rating")
         plt.show()
-    elif args.episodes:
+    elif args.mode == "episodes":
+        episodes_rated = episode[episode["parentTconst"] == id].join(ratings)
+        plt.title(title)
         sns.lineplot(x="episodeNumber", y="averageRating", data=episodes_rated)
         plt.xlabel("Episode number")
         plt.ylabel("Average episode rating")
         plt.show()
+    elif args.mode == "completion":
+        episode = episode[episode["parentTconst"] == id]
+        basics = load_basics(usecols=[0, 5, 7])
+        episode = basics.join(episode, how="inner")
+
+        episodes_in_season = episode[
+            (episode["seasonNumber"] == args.season)
+            & (episode["episodeNumber"] >= args.episode)
+        ]
+        next_seasons = episode[episode.fillna(0)["seasonNumber"] > args.season]
+        episodes_left = pd.concat([episodes_in_season, next_seasons])
+
+        episodes_left_count = episodes_left["episodeNumber"].shape[0]
+        episodes_count = episode.shape[0]
+
+        total_runtime = episode["runtimeMinutes"].sum()
+        remaining_runtime = episodes_left["runtimeMinutes"].sum()
+
+        years = sorted(episode["startYear"].dropna().unique())
+
+        print(
+            f"Completed {episodes_count - episodes_left_count} episodes"
+            f" with {episodes_left_count} left"
+            f" from {episodes_count} episodes total"
+            f" ({100-episodes_left_count/episodes_count*100:.2f}%)"
+        )
+        print(
+            f"Watched {total_runtime - remaining_runtime} minutes{hours_minutes(total_runtime - remaining_runtime)}"
+            f" with {remaining_runtime} minutes left{hours_minutes(remaining_runtime)}"
+            f" from {total_runtime} minutes total{hours_minutes(total_runtime - remaining_runtime)}"
+            f" ({100-remaining_runtime/total_runtime*100:.2f}%)"
+        )
+        print(f"Series episodes were released in {collapse_years(years)}")
+        print()
+        for season in sorted(episodes_left["seasonNumber"].unique()):
+            remaining_episodes_in_season = episodes_left[
+                episodes_left["seasonNumber"] == season
+            ]
+            remaining_episodes_in_season_runtime = remaining_episodes_in_season[
+                "runtimeMinutes"
+            ].sum()
+            span = collapse_years(
+                sorted(remaining_episodes_in_season["startYear"].dropna().unique())
+            )
+            print(
+                f"- In season {season} ({span}) there are {remaining_episodes_in_season.shape[0]} remaining episodes"
+                f" with total runtime of {remaining_episodes_in_season_runtime} minutes{hours_minutes(remaining_episodes_in_season_runtime)}"
+                f" ({remaining_episodes_in_season_runtime/total_runtime*100:.2f}% of whole series)"
+            )
     else:
-        error("Expected one of graph generation options")
+        error(f"Unknown mode: {args.mode}")
         exit(2)
 
 
@@ -139,18 +240,33 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Plots and statistics about TV series ratings using IMDb database"
     )
-
     parser.add_argument("-i", "--id", help="ID of the series")
     parser.add_argument("-n", "--name", help="Name of the series")
+    parser.add_argument("-v", "--verbose", action="count", default=0)
 
-    parser.add_argument("--box", help="Generate box plot", action="store_true")
-    parser.add_argument("--episodes", help="Generate line plot", action="store_true")
+    subparsers = parser.add_subparsers(required=True)
+
+    episodes = subparsers.add_parser("episodes", help="Generate line plot")
+    episodes.set_defaults(mode="episodes")
+
+    box = subparsers.add_parser("box", help="Generate box plot")
+    box.set_defaults(mode="box")
+
+    completion = subparsers.add_parser(
+        "completion", help="Calculate how much of given series was completed"
+    )
+    completion.set_defaults(mode="completion")
+    completion.add_argument(
+        "-s", "--season", help="Current season of given series", required=True, type=int
+    )
+    completion.add_argument(
+        "-e",
+        "--episode",
+        help="Current episode of given series",
+        required=True,
+        type=int,
+    )
 
     args = parser.parse_args()
-
-    if args.name is None and args.id is None:
-        error("Either name or id of the show was expected")
-        parser.print_usage(sys.stderr)
-        exit(2)
-
+    verbosity_level = args.verbose
     main(args)
